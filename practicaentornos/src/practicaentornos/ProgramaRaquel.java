c5266bd422bd698bc07261703989c574901e91a7
Fran <Fran@192.168.1.61>
2017-03-20 12:01:44 +0100

    primero...

---
+++ practicaentornos/src/practicaentornos/ProgramaRaquel.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programaraquel;
+
+/**
+ *
+ * @author usuario
+ */
+public class ProgramaRaquel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+        System.out.println("Porcentaje que quiere que gane el barsa 4%");
+        System.out.println("Porcentaje de gente que no quiere que se clasifique el Real Madrid 2%");
+    }
+    
+}
